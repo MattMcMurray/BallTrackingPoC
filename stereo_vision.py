7d01a77e42d7ae2d85d3883df1828334034aff97
Matt McMurray <mathieumcmurray@gmail.com>
2018-01-05 12:32:41 -0600

    Calibrate both cameras and return frames

--- stereo_vision.py
+++ stereo_vision.py
@@ -1,13 +1,10 @@
 ''' Proof of concept for DIY stereo vision '''
 
-import argparse
 import sys
+import os
 
 import cv2
-import imutils
-
 from matplotlib import pyplot as plt
-
 import numpy as np
 
 LEFT = 0
@@ -20,10 +17,62 @@ def wait_for_q():
         # if the 'q' key is pressed, stop the loop
         if key == ord("q"):
             break
+        elif key == ord("a"):
+            print('Abort')
+            sys.exit(0)
+
+def cache_calibration_arrays(ret, mtx, dist, rvecs, tvecs, file_prefix):
+    np.save(os.path.join('calibration', '{}_ret.npy'.format(file_prefix)), ret)
+    np.save(os.path.join('calibration', '{}_mtx.npy'.format(file_prefix)), mtx)
+    np.save(os.path.join('calibration', '{}_dist.npy'.format(file_prefix)), dist)
+    np.save(os.path.join('calibration', '{}_rvecs.npy'.format(file_prefix)), rvecs)
+    np.save(os.path.join('calibration', '{}_tvecs.npy'.format(file_prefix)), tvecs)
+
+def load_calibration_arrays(file_prefix):
+    try:
+        ret = np.load(os.path.join('calibration', '{}_ret.npy'.format(file_prefix)))
+        mtx = np.load(os.path.join('calibration', '{}_mtx.npy'.format(file_prefix)))
+        dist = np.load(os.path.join('calibration', '{}_dist.npy'.format(file_prefix)))
+        rvecs = np.load(os.path.join('calibration', '{}_rvecs.npy'.format(file_prefix)))
+        tvecs = np.load(os.path.join('calibration', '{}_tvecs.npy'.format(file_prefix)))
+
+        return ret, mtx, dist, rvecs, tvecs
+    except IOError as e:
+        print 'Could not load distortion matrices from disk...'
+        return None
+
+def capture_calibrated_shot():
+    cam1 = cv2.VideoCapture(0)
+    cam2 = cv2.VideoCapture(1)
+    grabbed1 = cam1.grab()
+    grabbed2 = cam2.grab()
+    if not grabbed1 or not grabbed2:
+        print('Could not grab frame for calibration')
+        sys.exit(1)
+
+    _, frame1 = cam1.retrieve()
+    _, frame2 = cam2.retrieve()
+
+    cam1.release()
+    cam2.release()
+
+    cv2.imshow('left', frame1)
+    cv2.imshow('right', frame2)
+
+    wait_for_q()
+
+    cv2.destroyAllWindows()
+
+    undst1 = calibrate_single_camera(frame1, 'l')
+    undst2 = calibrate_single_camera(frame2, 'r')
+
+    return undst1, undst2
 
 
-def calibrate_single_camera(cam, file_prefix):
 
+def calibrate_single_camera(frame, file_prefix):
+
+    # checkerboard inside corners
     num_rows = 7
     num_cols = 5
 
@@ -36,30 +85,30 @@ def calibrate_single_camera(cam, file_prefix):
     objpoints = []  # 3d point in real world space
     imgpoints = []  # 2d points in image plane.
 
-    (grabbed, frame) = cam.read()
+    calib_arrs = load_calibration_arrays(file_prefix)
 
-    if not grabbed:
-        print('Could not grab')
-        sys.exit(1)
+    if calib_arrs is None:
+        print('Buidling new distortion correction matrices...')
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (num_rows, num_cols), None)
 
-    ret, corners = cv2.findChessboardCorners(gray, (num_rows, num_cols), None)
+        if ret == True:
+            objpoints.append(objp)
 
-    if ret == True:
-        objpoints.append(objp)
+            cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners)
 
-        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners)
+            # Draw and display the corners
+            cv2.drawChessboardCorners(frame, (num_rows, num_cols), corners, ret)
 
-        # Draw and display the corners
-        cv2.drawChessboardCorners(frame, (num_rows, num_cols), corners, ret)
-        cv2.imshow('img', frame)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
+            objpoints, imgpoints, gray.shape[::-1], None, None)
 
-        wait_for_q()
-
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-        objpoints, imgpoints, gray.shape[::-1], None, None)
+        cache_calibration_arrays(ret, mtx, dist, rvecs, tvecs, file_prefix)
+    else:
+        print('Loaded cached arrays')
+        ret, mtx, dist, rvecs, tvecs = calib_arrs
 
     h,  w = frame.shape[:2]
     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
@@ -68,42 +117,21 @@ def calibrate_single_camera(cam, file_prefix):
     dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)
 
     # # crop the image
-    # x,y,w,h = roi
-    # dst = dst[y:y+h, x:x+w]
+    x,y,w,h = roi
+    dst = dst[y:y+h, x:x+w]
 
-    cv2.imwrite('undistorted.png', dst)
+    cv2.imshow('distorted_{}'.format(file_prefix), frame)
+    cv2.imshow('undistorted_{}'.format(file_prefix), dst)
     wait_for_q()
 
     cv2.destroyAllWindows()
-    cam.release()
 
+    return dst
 
-def run():
-    cam1 = cv2.VideoCapture(0)
-    cam2 = cv2.VideoCapture(1)
+def build_depth_map(left, right):
 
-    cam1.grab()
-    cam2.grab()
-
-    _, frame1 = cam1.retrieve()
-    _, frame2 = cam2.retrieve()
-
-    imleft = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    imright = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('left', imleft)
-    cv2.imshow('right', imright)
-
-    while True:
-        key = cv2.waitKey(1) & 0xFF
-
-        # if the 'q' key is pressed, stop the loop
-        if key == ord("q"):
-            break
-
-    cv2.destroyAllWindows()
-    cam1.release()
-    cam2.release()
+    imleft = cv2.cvtColor(left, cv2.COLOR_BGR2GRAY)
+    imright = cv2.cvtColor(right, cv2.COLOR_BGR2GRAY)
 
     stereo = cv2.StereoBM_create(numDisparities=32, blockSize=11)
 
@@ -115,4 +143,4 @@ def run():
     return
 
 if __name__ == '__main__':
-    calibrate_single_camera(cv2.VideoCapture(0), 'r')
+    left, right = capture_calibrated_shot()
